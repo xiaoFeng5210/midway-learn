93a9620baefd7c4a761b963ee2b091a1989986b6
Eadweard <804875728@qq.com>
2023-11-30 13:06:16 +0800

    天气示例

--- src/controller/weather.controller.ts
+++ src/controller/weather.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get, Query } from '@midwayjs/core';
+import { Controller, Get, Inject, Query } from '@midwayjs/core';
+import { WeatherService } from '../service/weather.service';
+import { WeatherInfo } from '../interface';
 
 @Controller('/')
 export class WeatherController {
+  @Inject()
+  weatherService: WeatherService;
+
   @Get('/weather')
-  async getWeatherInfo(@Query('cityId') cityId: string): Promise<string> {
-    return cityId;
+  async getWeatherInfo(@Query('cityId') cityId: string): Promise<WeatherInfo> {
+    return this.weatherService.getWeather(cityId);
   }
 }
