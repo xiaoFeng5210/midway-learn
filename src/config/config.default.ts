df7e0e175c06516a24d9d13eaf6780008ad9a3ed
Eadweard <804875728@qq.com>
2023-11-30 12:38:58 +0800

    init

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1701318846511_3777',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
